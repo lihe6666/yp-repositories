6222f2a4f9410fc9cd7eba71806e1c8c67ee60c5
李大壮 <lihe6666@163.com>
2024-04-26 01:46:35 -0700

    Change MainActivity

--- app/src/main/java/com/yp2048/repositories/MainActivity.kt
+++ app/src/main/java/com/yp2048/repositories/MainActivity.kt
@@ -3,11 +3,14 @@ package com.yp2048.repositories
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.yp2048.repositories.ui.theme.RepositoriesTheme
@@ -31,13 +34,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "智能无人仓管理系统-前端Android",
+            modifier = modifier
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     RepositoriesTheme {
