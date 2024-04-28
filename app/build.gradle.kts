8efa7d192e1cf97d07840e72522979fea0d7e02f
李大壮 <lihe6666@163.com>
2024-04-27 21:56:40 -0700

    Add camera preview.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.material3)
 
     // Kotlin
     implementation(libs.androidx.navigation.fragment.ktx)
@@ -63,6 +64,22 @@ dependencies {
 
     // Jetpack Compose Integration
     implementation(libs.androidx.navigation.compose)
+
+
+    // CameraX core library using the camera2 implementation
+    // The following line is optional, as the core library is included indirectly by camera-camera2
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    // If you want to additionally use the CameraX Lifecycle library
+    implementation(libs.androidx.camera.lifecycle)
+    // If you want to additionally use the CameraX VideoCapture library
+    implementation(libs.androidx.camera.video)
+    // If you want to additionally use the CameraX View class
+    implementation(libs.androidx.camera.view)
+    // If you want to additionally add CameraX ML Kit Vision Integration
+    // implementation(libs.androidx.camera.mlkit.vision)
+    // If you want to additionally use the CameraX Extensions library
+    implementation(libs.androidx.camera.extensions)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/com/yp2048/repositories/MainActivity.kt
+++ app/src/main/java/com/yp2048/repositories/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.yp2048.repositories
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -21,8 +32,28 @@ import com.yp2048.repositories.ui.theme.RepositoriesTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!hasRequiredPermissions()) {
+            // Permissions granted, proceed with the app
+            ActivityCompat.requestPermissions(
+                this,
+                CAMERA_PERMISSIONS,
+                0
+            )
+        }
+
         setContent {
             RepositoriesTheme {
+
+                val controller = remember {
+                    LifecycleCameraController(this).apply {
+                        setEnabledUseCases(
+                            CameraController.IMAGE_CAPTURE or
+                                    CameraController.VIDEO_CAPTURE
+                        )
+                    }
+                }
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -31,14 +62,50 @@ class MainActivity : ComponentActivity() {
                     val navController = rememberNavController()
                     NavHost(navController, startDestination = "main") {
                         composable("main") {
-                            Greeting()
+                            CameraPreview(
+                                controller = controller,
+                                modifier = Modifier.fillMaxSize()
+                            )
                         }
                         // 在这里添加更多的 composable
                     }
+
                 }
             }
         }
     }
+
+    private fun hasRequiredPermissions(): Boolean {
+        return CAMERA_PERMISSIONS.all {
+            ActivityCompat.checkSelfPermission(
+                applicationContext,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    companion object {
+        private val CAMERA_PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+        )
+    }
+}
+
+@Composable
+fun CameraPreview(
+    controller: LifecycleCameraController,
+    modifier: Modifier = Modifier
+) {
+    // 在这里添加相机预览的 composable
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    AndroidView(factory = {
+        PreviewView(it).apply {
+            this.controller = controller
+            controller.bindToLifecycle(lifecycleOwner)
+        }
+    }, modifier = modifier)
 }
 
 @Composable
