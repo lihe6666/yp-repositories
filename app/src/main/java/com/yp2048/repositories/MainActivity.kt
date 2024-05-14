3a2d83a4d769dbc89d37bdc45a97ae4f8467df3b
李大壮 <lihe6666@163.com>
2024-05-14 03:26:43 -0700

    Updated: Request permission.

--- app/src/main/java/com/yp2048/repositories/MainActivity.kt
+++ app/src/main/java/com/yp2048/repositories/MainActivity.kt
@@ -3,8 +3,10 @@ package com.yp2048.repositories
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,16 +16,28 @@ import com.yp2048.repositories.presentation.Navigation
 import com.yp2048.repositories.ui.theme.RepositoriesTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val permissionsLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            if (permissions.all { it.value }) {
+                // 如果用户授予了所需的权限，初始化应用
+            } else {
+                // 如果用户拒绝了权限请求，显示一个提示并退出应用
+                Toast.makeText(
+                    this,
+                    "需要授权相机和录音权限",
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         if (!hasRequiredPermissions()) {
             // Permissions granted, proceed with the app
-            ActivityCompat.requestPermissions(
-                applicationContext as MainActivity,
-                CAMERA_PERMISSIONS,
-                0
-            )
+            requestPermissions()
         }
 
         setContent {
@@ -37,6 +51,11 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun requestPermissions() {
+        // 请求所需的权限
+        permissionsLauncher.launch(CAMERA_PERMISSIONS)
     }
 
     private fun hasRequiredPermissions(): Boolean {
